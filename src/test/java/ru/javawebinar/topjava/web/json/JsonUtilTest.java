74306737876eeecc3e2bc7251839cba3daab9153
Viterg <viterg@bk.ru>
2020-11-24 18:20:30 +0300

    JsonUtilTest fix

--- src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web.json;
 
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import static ru.javawebinar.topjava.MealTestData.*;
 class JsonUtilTest {
 
     @Test
-    void readWriteValue() throws Exception {
+    void readWriteValue() {
         String json = JsonUtil.writeValue(adminMeal1);
         System.out.println(json);
         Meal meal = JsonUtil.readValue(json, Meal.class);
@@ -18,10 +19,10 @@ class JsonUtilTest {
     }
 
     @Test
-    void readWriteValues() throws Exception {
+    void readWriteValues() {
         String json = JsonUtil.writeValue(meals);
         System.out.println(json);
         List<Meal> meals = JsonUtil.readValues(json, Meal.class);
-        MEAL_MATCHER.assertMatch(meals, meals);
+        MEAL_MATCHER.assertMatch(meals, MealTestData.meals);
     }
 }
